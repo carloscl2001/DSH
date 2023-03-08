a78f662e727dae42218b76f098456f4da5df2ee8
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-03-08 22:38:04 +0100

    CAMBIOS

--- Projecto 2/Assets/Scripts/movimientoBala.cs
+++ Projecto 2/Assets/Scripts/movimientoBala.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class movimientoBala : MonoBehaviour
 {
-    public float velocidad = 5.0f;
+    public float velocidad;
     public float valorHerida = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        velocidad = 75.0f;
     }
 
     // Update is called once per frame
